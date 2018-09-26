36d9968c1099bf8e3d14fe219e591487813dbe59
azjezz <azjezz@azjezz.me>
2018-09-27 00:39:46 +0100

    Update Copyright.

--- tests/ConfigProviderTest.php
+++ tests/ConfigProviderTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This File is Part of the Validus Translation package.
+ *
+ * @copyright (c) 2018 Validus <https://github.com/ValidusPHP/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
 
